f8a2d39b6a5b070ad4fd20f43c900988bc21ca85
Nayan Chaturvedi <123476375+Nayan-Chaturvedi@users.noreply.github.com>
2025-12-06 13:25:39 +0530

    Update PrimeNumber.java

--- CoreJava/src/extra_/PrimeNumber.java
+++ CoreJava/src/extra_/PrimeNumber.java
@@ -62,3 +62,29 @@ class Main {
          }
     }
 }
+
+// Check Array is sorted or not
+class Main {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 2};
+        boolean flag=true;
+        for(int i=0; i<arr.length-1; i++)
+        {
+            
+            if(arr[i]>arr[i+1])
+            {
+                flag = false;
+                break;
+            }
+        }
+        if(flag){
+            System.out.println("Array is sorted");
+        }
+        else
+        {
+            System.out.println("Array is not sorted");
+        }
+
+       
+    }
+}
