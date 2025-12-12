be8de72e4889a74c178d286e44e65a421d045c96
Nayan Chaturvedi <123476375+Nayan-Chaturvedi@users.noreply.github.com>
2025-12-12 10:52:31 +0530

    Add program to sort negative elements in an array
    Implement a program to sort negative elements at the start of the array.

--- CoreJava/src/extra_/SortingName.java
+++ CoreJava/src/extra_/SortingName.java
@@ -154,3 +154,42 @@ public class Program_1 {
 
 }
 
+// All negative element in starting
+package JavaBasicProgram;
+
+public class Program_1 {
+
+	public static void main(String[] args) {
+		
+		int arr[]= {-1, -20, 30, 40, 50, -8};
+		int temp[]= {0, 0, 0, 0, 0, 0};
+		int j=0;
+		
+		for (int i = 0; i < arr.length; i++)
+		{
+			if(arr[i]<0)
+			{
+				temp[j++]=arr[i];
+			}
+			
+		}
+		
+		for (int i = 0; i < arr.length; i++)
+		{
+			if(arr[i]>0)
+			{
+				temp[j++]=arr[i];
+			}
+			
+		}
+		
+		for(int i=0; i<temp.length; i++)
+		{
+			System.out.println(temp[i]);
+		}
+		
+	}
+
+}
+
+
